c6258c862eb5b55d2b58a717d66a0cc153ff7129
Hieu Van Le <hieule20901@gmail.com>
2020-07-25 21:17:29 -0500

    add more info form

--- src/components/details-form/details-form.js
+++ src/components/details-form/details-form.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./details-form.css";
 import { v4 as uuidv4 } from "uuid";
 
-import { Collapse, Form, Input, DatePicker, Button } from "antd";
+import { Collapse, Form, Input, DatePicker, Button, Select } from "antd";
 import { CloseCircleOutlined } from "@ant-design/icons";
 
 const { TextArea } = Input;
+const { Option } = Select;
 const { MonthPicker, RangePicker } = DatePicker;
 
 const { Panel } = Collapse;
@@ -42,15 +43,6 @@ const DetailsForm = () => {
       city: "",
       description: "",
     },
-    {
-      id: uuidv4(),
-      school: "(Not specified)",
-      degree: "",
-      startDate: now,
-      endDate: now,
-      city: "",
-      description: "",
-    },
   ]);
   const addEducation = () => {
     setEducations([
@@ -66,13 +58,71 @@ const DetailsForm = () => {
       },
     ]);
   };
-  const deleteEducation = (id) => {
-    const filterSelected = () => {
-      setEducations(educations.filter((education) => education.id !== id));
+  const deleteEducation = (educationId) => {
+    const filterSelectedEducation = () => {
+      setEducations(
+        educations.filter((education) => education.id !== educationId)
+      );
     };
-    return <CloseCircleOutlined onClick={filterSelected} />;
+    return <CloseCircleOutlined onClick={filterSelectedEducation} />;
   };
-  console.log(educations);
+  const [skills, setSkills] = React.useState([
+    {
+      id: uuidv4(),
+      skill: "(Not specified)",
+      level: "Novice",
+    },
+  ]);
+  const addSkill = () => {
+    setSkills([
+      ...skills,
+      {
+        id: uuidv4(),
+        skill: "(Not specified)",
+        level: "Novice",
+      },
+    ]);
+  };
+  const deleteSkill = (skillId) => {
+    const removeSkill = () => {
+      setSkills(skills.filter((skill) => skill.id !== skillId));
+    };
+    return <CloseCircleOutlined onClick={removeSkill} />;
+  };
+  const [experience, setExperience] = React.useState([
+    {
+      id: uuidv4(),
+      jobTitle: "(Not specified)",
+      Employer: "",
+      startDate: now,
+      endDate: now,
+      city: "",
+      description: "",
+    },
+  ]);
+  const addExperience = () => {
+    setExperience([
+      ...experience,
+      {
+        id: uuidv4(),
+        jobTitle: "(Not specified)",
+        employer: "",
+        startDate: now,
+        endDate: now,
+        city: "",
+        description: "",
+      },
+    ]);
+  };
+  const deleteExperience = (experienceId) => {
+    const filterSelectedEducation = () => {
+      setExperience(
+        educations.filter((education) => education.id !== experienceId)
+      );
+    };
+    return <CloseCircleOutlined onClick={filterSelectedEducation} />;
+  };
+
   return (
     <div style={{ marginTop: "1rem" }}>
       <h3>Education</h3>
@@ -129,7 +179,106 @@ const DetailsForm = () => {
         + Add education
       </Button>
       <h3 style={{ marginTop: "1rem" }}>Technical Skills</h3>
-      <Collapse></Collapse>
+      {skills.length !== 0 ? (
+        <Collapse>
+          {skills.map((skill) => (
+            <Panel
+              key={skill.id}
+              header={`${skill.skill} - ${skill.level}`}
+              extra={deleteSkill(skill.id)}
+            >
+              <div style={{ display: "flex" }}>
+                <Form layout="vertical" {...formItemLayout}>
+                  <Form.Item label="Skill:">
+                    <Input name="skill" />
+                  </Form.Item>
+                </Form>
+                <Form layout="vertical" {...formItemLayout}>
+                  <Form.Item label="Level:">
+                    <Select>
+                      <Option value="1">Novice</Option>
+                      <Option value="2">Beginner</Option>
+                      <Option value="3">Skillful</Option>
+                      <Option value="4">Experienced</Option>
+                      <Option value="5">Expert</Option>
+                    </Select>
+                  </Form.Item>
+                </Form>
+              </div>
+            </Panel>
+          ))}
+        </Collapse>
+      ) : null}
+      <Button
+        type="dashed"
+        block
+        style={{
+          marginTop: "1rem",
+          color: "#40BCFF",
+          borderColor: "#40BCFF",
+        }}
+        onClick={addSkill}
+      >
+        + Add skill
+      </Button>
+      <h3 style={{ marginTop: "1rem" }}>Work Experience</h3>
+      {experience.length !== 0 ? (
+        <Collapse>
+          {experience.map((exp, index) => (
+            <Panel
+              key={exp.id}
+              header={`${exp.jobTitle} | ${exp.startDate} - ${exp.endDate}`}
+              extra={deleteExperience(exp.id)}
+            >
+              <div style={{ display: "flex" }}>
+                <Form layout="vertical" {...formItemLayout}>
+                  <Form.Item label="Job title:">
+                    <Input name="jobTitle" />
+                  </Form.Item>
+                  <Form.Item label="Start & End date:" {...rangeConfig}>
+                    <RangePicker />
+                  </Form.Item>
+                </Form>
+                <Form layout="vertical" {...formItemLayout}>
+                  <Form.Item label="Employer:">
+                    <Input name="employer" />
+                  </Form.Item>
+                  <Form.Item label="City:">
+                    <Input name="city" />
+                  </Form.Item>
+                </Form>
+              </div>
+              <p
+                style={{
+                  fontSize: "14px",
+                  lineHeight: "22px",
+                  color: "rgba(0,0,0,0.85)",
+                }}
+              >
+                Description:{" "}
+              </p>
+              <TextArea
+                rows={4}
+                style={{ width: "92%" }}
+                placeholder="e.g. Using influxDB for real-time data to reduce efficient cost..."
+              />
+            </Panel>
+          ))}
+        </Collapse>
+      ) : null}
+      <Button
+        type="dashed"
+        block
+        style={{
+          marginTop: "1rem",
+          color: "#40BCFF",
+          borderColor: "#40BCFF",
+          marginBottom: "2rem",
+        }}
+        onClick={addExperience}
+      >
+        + Add experience
+      </Button>
     </div>
   );
 };
